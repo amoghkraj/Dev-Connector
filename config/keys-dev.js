8f62de9e68a65b6ace0ca77461478b30a6305fdd
Amogh <amogha.kraj@gmail.com>
2018-12-28 20:37:57 +0530

    Configured pacage.json and keys for the producion env

---
+++ config/keys-dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin:admin@cluster0-skv4p.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
+};
--- server.js
+++ server.js
@@ -33,4 +33,14 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => console.log(`Server running on ${port}`));
